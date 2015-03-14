d79415ffb1e57a0483a4f544f163f64591c66cd4
Filip Gutica <fgutica@gmail.com>
2015-03-13 18:44:17 -0700

    split server into server.h and server.cpp added comments
    Signed-off-by: Filip Gutica <fgutica@gmail.com>

---
+++ Server/server.h
@@ -0,0 +1,42 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+
+#include <cstdio>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <cstring>
+#include <arpa/inet.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <string>
+#include <string.h>
+
+#include "xmlmanip.h"
+
+//defines
+#define SERVER_TCP_PORT 7000	// Default port
+#define BUFLEN	1024				//Buffer length
+
+struct pdata
+{
+  char* plong;
+  char* plat;
+  char* pip;
+  char* ptime;
+  char* pmac;
+};
+
+//prototypes
+
+void readFromClient(int client_socket);
+pdata rawToPData(char* str);
+void sig_handler (int sig);
+Location pDataToLocation (pdata data);
+
+#endif
